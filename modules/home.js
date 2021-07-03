62fbd8f5d20307e00ecfe69e9fa50ab2c5762f9e
ssaadakhtarr <muhammadsaadakhtar19@gmail.com>
2021-07-03 20:30:28 +0500

    few

--- modules/home.js
+++ modules/home.js
@@ -1,33 +1,29 @@
 const db = require("../db/connection");
 
 exports.getHomeData = function (req, res) {
-    
-    var newArr = [];
-    db.query(`SELECT * FROM blog`, (err, result) => {
+  var newArr = [];
+  db.query(`SELECT * FROM blog`, (err, result) => {
+    result.map((i, index) => {
+      db.query(
+        `SELECT username FROM users WHERE id = ?`,
+        [i.userId],
+        (err, result1) => {
+          //    console.log(result1);
+          newArr.push({ ...i, username: result1[0].username });
+          //    console.log(newArr);
+          // if (result.length === index + 1) {
+          //   // console.log(newArr);
+          // } else {
+            db.query(`SELECT * FROM rooms`, (err, result_1) => {
+              // console.log(err);
+              // console.log(result_1);
+              console.log(newArr);
+              console.log(result_1);
+              res.send({ newArr, result_1 });
+            });
+          }
         
-        result.map((i, index) => {
-          db.query(
-            `SELECT username FROM users WHERE id = ?`,
-            [i.userId],
-            (err, result1) => {
-              //    console.log(result1);
-              newArr.push({ ...i, username: result1[0].username });
-              //    console.log(newArr);
-              if (result.length === index + 1) {
-                console.log(newArr);
-                
-              }
-              else{
-                db.query(`SELECT * FROM rooms`, (err, result_1) => {
-                  // console.log(err);
-                  // console.log(result_1);
-                  res.send({newArr, result_1});
-              })
-              }
-            }
-          );
-
-         
-        });
-      });
-}
+      );
+    });
+  });
+};
