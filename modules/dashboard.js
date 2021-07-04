36785a5db8c2471f5a6bb1afe3ed368ecca38694
ssaadakhtarr <muhammadsaadakhtar19@gmail.com>
2021-07-04 21:57:02 +0500

    user

--- modules/dashboard.js
+++ modules/dashboard.js
@@ -3,14 +3,19 @@ const db = require("../db/connection");
 exports.getDashboard = function (req, res) {
   db.query(`SELECT COUNT(*) FROM users`, (err, results) => {
     db.query(
-      `SELECT rank,level,points FROM users WHERE id=${req.params.id}`,
+      `SELECT rank,level,points,title,firstName,lastName,username,email,dateOfBirth, gender, occupation, areaOfInterest, summary, twitter, instagram, github, linkedin FROM users WHERE id=${req.params.id}`,
       (err, results1) => {
-        const { rank, points, level } = results1[0];
+        const { rank, points, level, title, firstName, lastName, username,email,dateOfBirth, gender, occupation, areaOfInterest, summary, twitter, instagram, github, linkedin } = results1[0];
         res.send({
           users: results[0]["COUNT(*)"],
           rank,
           points,
           level,
+          title,
+          firstName,
+          lastName,
+          username,
+          email,dateOfBirth, gender, occupation, areaOfInterest, summary, twitter, instagram, github, linkedin,
         });
       }
     );
