d9f3f7e327b409236d8ed2b736ca423fed6d02e1
Areeb Nadeem <areebnadeem29@gmail.com>
2021-07-05 14:12:58 +0500

    ch

--- modules/leaderboard.js
+++ modules/leaderboard.js
@@ -1,6 +1,6 @@
 const db = require("../db/connection");
 exports.getLeaderboard = function (req, res){
-    db.query(`SELECT rank, username,points,level FROM users`, (err, result) => {
+    db.query(`SELECT rank, username,points,level FROM users ORDER BY rank`, (err, result) => {
         console.log(result);
         res.send(result);
     })
