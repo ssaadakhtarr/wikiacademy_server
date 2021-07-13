496814faf3e958f7cda73a097a9671f0e0c8d524
ssaadakhtarr <muhammadsaadakhtar19@gmail.com>
2021-07-13 21:48:24 +0500

    heroku

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "http://localhost:5000", "https://wikisecurityacademy.azurewebsite.net", "https://wikisecurityacademy.surge.sh"],
     methods: ["GET", "POST"],
     credentials: true,
   })
