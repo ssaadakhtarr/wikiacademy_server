6dcbf699cca4beed37d748b24390b4b4827571f7
ssaadakhtarr <muhammadsaadakhtar19@gmail.com>
2021-07-23 10:59:08 +0500

    bug fixed

--- modules/flags.js
+++ modules/flags.js
@@ -3,7 +3,7 @@ const db = require("../db/connection");
 exports.checkFlag = function (req, res) {
     const flag = req.body.submitFlag;
     const userId = req.body.userId;
-    var exists = true;
+    var exists = false;
     console.log(flag, userId);
     db.query(`SELECT id from flags WHERE flagValue = ?`, [flag], (err, result) => {
         console.log(err);
@@ -15,15 +15,16 @@ exports.checkFlag = function (req, res) {
                 if (result1.length > 0) {
                     result1.map((i)=>{
                         if (i.flagId === result[0].id) {
-                            res.json({status: "Already submitted"})
                             exists = true;
-                        } else {
-                            exists = false;
-                        }
+                            res.json({status: "Already submitted"})
+                            
+                            
+                        } 
                     })
                 } 
-                
+                console.log("else after")
                 if (exists === false) {
+                    console.log("if here")
                     db.query(`INSERT INTO userflags (flagId, userId) VALUES (?, ?)`, [result[0].id, userId], (err, result2) => {
                         console.log(err);
                         console.log(result2);
@@ -32,6 +33,7 @@ exports.checkFlag = function (req, res) {
                 }
             })
         } else {
+            console.log("else after after")
             res.json({status: "Invalid flag"})
         }
     })
