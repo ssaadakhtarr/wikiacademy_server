164a1fea2e7748e7cf1bbbb00ec6183de67b34a1
Areeb Nadeem <areebnadeem29@gmail.com>
2021-07-28 17:36:13 +0500

    c

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,20 @@
 const mysql = require("mysql");
 
-const db = mysql.createConnection({
-  user: "root",
-  host: "localhost",
-  password: "",
+// const db = mysql.createConnection({
+//   user: "areeb@areeb",
+//   host: "localhost",
+//   password: "",
+//   database: "wikisecdb",
+//   multipleStatements: true,
+
+// });
+
+var db = mysql.createConnection({
+  host: "areeb.mysql.database.azure.com",
+  user: "areeb@areeb",
+  password: "SE0451980nadeem",
   database: "wikisecdb",
-  multipleStatements: true,
+  port: 3306,
 });
 
 module.exports = db;
