18d9efcd7b0b1e42931e9637f00bb7efce250e1e
ssaadakhtarr <muhammadsaadakhtar19@gmail.com>
2021-07-30 02:21:16 +0500

    added allow origin header

--- db/connection.js
+++ db/connection.js
@@ -1,23 +1,23 @@
 const mysql = require("mysql");
 
-// const db = mysql.createConnection({
-//   user: "root",
-//   host: "localhost",
-//   password: "",
-//   database: "wikisecdb",
-//   multipleStatements: true,
-// });
-
-
-
-var db = mysql.createConnection({
-  host: "areeb.mysql.database.azure.com",
-  user: "areeb@areeb",
-  password: "SE0451980nadeem",
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "",
   database: "wikisecdb",
-  port: 3306,
   multipleStatements: true,
 });
+
+
+
+// var db = mysql.createConnection({
+//   host: "areeb.mysql.database.azure.com",
+//   user: "areeb@areeb",
+//   password: "SE0451980nadeem",
+//   database: "wikisecdb",
+//   port: 3306,
+//   multipleStatements: true,
+// });
 
 db.connect((err)=> {
   if(!err)
